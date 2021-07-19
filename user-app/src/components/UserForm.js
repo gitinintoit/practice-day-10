f6cb500e186841fb54dceba97ed4f3168f970494
Ravi Shyam <ravi.shyam@timesinternet.in>
2021-07-19 12:43:11 +0530

    Adding age feild in form

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -1,19 +1,30 @@
 import { useState }
-from 'react'
-export default function UserForm(){
+    from 'react'
+export default function UserForm() {
 
-    const [userForm,setUserform]=useState({firstname:"Ravi"}) //hook function
+    const [userForm, setUserform] = useState({ 
+        firstname: "Ravi", 
+        age: "23" }) //hook function
 
-    const handleEvent=function(event){
-        console.log(event);
-        setUserform({...userForm,firstname:event.target.value});
+    const handleEventName = function (event) {
+        setUserform({ ...userForm, firstname: event.target.value });
     }
-    return(
+    const handleEventAge = function (event) {
+        setUserform({ ...userForm, age: event.target.value });
+    }
+
+    const save = function (event) {
+        console.log("User first name: "+userForm.firstname);
+        console.log("User age: "+userForm.age);
+
+    }
+    return (
         <div>
             <h3>Create User form</h3>
-            <input value={userForm.firstname} onChange={handleEvent}>
-
+            <input value={userForm.firstname} onChange={handleEventName}>
             </input>
+            <input value={userForm.age} onChange={handleEventAge}></input>
+            <button onClick={save}>Save</button>
         </div>
     );
 }
