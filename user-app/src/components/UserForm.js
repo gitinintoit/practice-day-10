caabcb8071443ddcfd017bbf7e3856517b28551d
Ravi Shyam <ravi.shyam@timesinternet.in>
2021-07-19 12:19:39 +0530

    Adding two way data binding

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -3,10 +3,15 @@ from 'react'
 export default function UserForm(){
 
     const [userForm,setUserform]=useState({firstname:"Ravi"}) //hook function
+
+    const handleEvent=function(event){
+        console.log(event);
+        setUserform({...userForm,firstname:event.target.value});
+    }
     return(
         <div>
             <h3>Create User form</h3>
-            <input value={userForm.firstname}>
+            <input value={userForm.firstname} onChange={handleEvent}>
 
             </input>
         </div>
