002fe4fabbaefcf3db48da176785db811f6d71f6
gitinintoit <ravi.shyam@timesinternet.in>
2021-07-19 14:23:26 +0530

    Adding a common event handler

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -6,13 +6,9 @@ export default function UserForm() {
         firstname: "Ravi", 
         age: "23" }) //hook function
 
-    const handleEventName = function (event) {
-        setUserform({ ...userForm, firstname: event.target.value });
+    const handleEvent = function (event) {
+        setUserform({ ...userForm,[event.target.name]: event.target.value });
     }
-    const handleEventAge = function (event) {
-        setUserform({ ...userForm, age: event.target.value });
-    }
-
     const save = function (event) {
         console.log("User first name: "+userForm.firstname);
         console.log("User age: "+userForm.age);
@@ -21,9 +17,9 @@ export default function UserForm() {
     return (
         <div>
             <h3>Create User form</h3>
-            <input value={userForm.firstname} onChange={handleEventName}>
+            <input name='firstname' value={userForm.firstname} onChange={handleEvent}>
             </input>
-            <input value={userForm.age} onChange={handleEventAge}></input>
+            <input name='age'value={userForm.age} onChange={handleEvent}></input>
             <button onClick={save}>Save</button>
         </div>
     );
