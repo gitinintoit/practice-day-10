85ba4918d7aac22507eaa6fc7606f818afcb58b9
Ravi Shyam <ravi.shyam@timesinternet.in>
2021-07-20 09:30:04 +0530

    Adding left over file for day-10

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -1,17 +1,23 @@
 import { useState }
     from 'react'
+import axios from "axios"
 export default function UserForm() {
 
     const [userForm, setUserform] = useState({ 
         firstname: "Ravi", 
-        age: "23" }) //hook function
+        age: "23",
+        joiningDate: "" }) //hook function
 
     const handleEvent = function (event) {
         setUserform({ ...userForm,[event.target.name]: event.target.value });
     }
     const save = function (event) {
-        console.log("User first name: "+userForm.firstname);
-        console.log("User age: "+userForm.age);
+      console.log("User first name: "+userForm.firstname);
+      console.log("User age: "+userForm.age);
+      const promise=axios.post("http://localhost:4200/users", userForm);
+      promise.then(function(response){
+          console.log(response);
+      })
 
     }
     return (
@@ -20,6 +26,7 @@ export default function UserForm() {
             <input name='firstname' value={userForm.firstname} onChange={handleEvent}>
             </input>
             <input name='age'value={userForm.age} onChange={handleEvent}></input>
+             <br/>Joining date: <input name='joiningDate'type="date"value={userForm.joiningDate}  onChange={handleEvent}></input>
             <button onClick={save}>Save</button>
         </div>
     );
