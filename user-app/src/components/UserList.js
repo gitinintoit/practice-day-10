84f72015bc223e8e85dafdef8ae92ca3b09dee1d
Ravi Shyam <ravi.shyam@timesinternet.in>
2021-07-20 10:51:19 +0530

    Adding userlist display functionality

--- user-app/src/components/UserList.js
+++ user-app/src/components/UserList.js
@@ -1,22 +1,41 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export default function UserList() {
     const [users, setUsers] = useState([]);
+    useEffect(function () {
+        if(users.length!=0){
+            return;
+        }
+        console.log("called................")
+        const promise = axios.get("http://localhost:4200/users");
+        promise.then(function (response) {
+            setUsers(response.data)
+        })
+    })
     return (
 
         <div>
             <table className="table table-bordered table-hover table-responsive">
                 <thead>git
                     <tr>
-                        <td> FirstName</td>
-                        <td>Age</td>
+                        <th> FirstName</th>
+                        <th>Age</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Ravi </td>
-                        <td>23</td>
-                    </tr>
+                    {users.map(function (user, index) {
+                        return (
+                            <tr>
+                                <td>
+                                    {user.firstname}
+                                </td>
+                                <td>
+                                    {user.age}
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
 
