212c9f8a60235e75031ed839755448c7e0cf31e6
Ravi Shyam <ravi.shyam@timesinternet.in>
2021-07-20 10:18:30 +0530

    Adding user list to page

---
+++ user-app/src/components/UserList.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export default function UserList() {
+    const [users, setUsers] = useState([]);
+    return (
+
+        <div>
+            <table className="table table-bordered table-hover table-responsive">
+                <thead>
+                    <tr>
+                        <td> FirstName</td>
+                        <td>Age</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Ravi </td>
+                        <td>23</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
