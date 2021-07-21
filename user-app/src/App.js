ea51b8a2d582cdd9358f159d021a1ce5c7e1224b
gitinintoit <ravi.shyam@timesinternet.in>
2021-07-21 14:17:29 +0530

    Adding routes for various components in app

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,30 +1,61 @@
 // import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
 import UserForm from './components/UserForm'
 import UserList from './components/UserList'
-import  '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import About from './components/About';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
+    <Router>
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              
+            </div>
+            <ul className="nav navbar-nav">
+              <li>
+                <Link to="/">Home </Link>
+              </li>
+              <li>
+                <Link to="/about">About </Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
+
+        {
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Message />
+            </Route>
+            <Route path="/">
+              <UserForm />
+              <UserList />
+            </Route>
+          </Switch>
+        }
+      </div>
+    </Router>
+
+    // <div>
+    //   <UserForm />
+    //   <UserList />
     // </div>
-    <div>
-      <UserForm/>
-      <UserList/>
-    </div>
   );
 }
 
